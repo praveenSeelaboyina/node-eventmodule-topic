e6f7a695263469dde49e4f5c0d202e42d0d81e14
santhoshreddyvw <santhosh.v@voltuswave.com>
2024-02-08 12:30:43 +0530

    event module 2

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const emitter=new EventModule();
 /* here EventModule is the class it 
 can contain the different types of methods and objecrts */
 
-emitter.on("message",function(){   //emitter.on anedi event ni register chesthadi on anedi method  
-    console.log('meassage logged')
+emitter.on("message",(arg)=>{   //emitter.on anedi event ni register chesthadi on anedi method  
+    console.log('meassage logged',arg)
 })
 
-emitter.emit("message") 
+emitter.emit("message",{id:"1"}) 
 // emit() it is make the noise,produce something.and we are signalling event happend
 // ippudu cli lo node app.js run chesthe emi print kadu endukante ikkada emit() anedi hust event ni raise chesthadi 
 
+/* alage eppudaithe manam event ni call chesthamo adi some id,url ni return chesthadi
+danni manam message pakkana oka object dwara pamputhamu. */
